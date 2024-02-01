cc3f72e90f3fba229939039d48fc7b18f150061a
George Matos <geodm20@gmail.com>
2024-02-01 14:27:04 -0400

    Hero description modified by suggestions. Skills updated acoording to my desired stack. Added Star Wars project in Projects section.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hello, I'm <span className={styles.span}>George</span></h1>
-                <p className={styles.description}>I'm a React front-end developer. I also domain Python (which I love) and SQL. Since I really like everything about web development, I'm also looking to develop myself as backend developer to get full-stack roles. I am a fast learner, detail-oriented, and dedicated to producing high-quality work that meets the needs of clients and end-users. Reach out to learn more about me!</p>
+                <p className={styles.description}>I'm a React front-end developer. Since I really like everything about web development, I'm looking to develop myself as backend developer as well to get full-stack roles, leveraging technologies like the MERN stack or others. I'm also a Python developer, which is my favorite programming language. I am a fast learner, detail-oriented, and deeply committed to delivering top-notch solutions that meets the needs of clients and end-users. Reach out to learn more about me!</p>
                 <a href="mailto:geodm20@gmail.com" className={styles.contactBtn}>Contact Me</a> {/* Decide wether keep my email here or not */}
             </div>
             <img src={getImageUrl("hero/George.png")} alt="George photo" className={styles.heroImg}/>
