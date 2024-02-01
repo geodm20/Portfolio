44193167bb4f055b75c926a4cfeda00f9fe3607b
George Matos <geodm20@gmail.com>
2024-02-01 14:47:35 -0400

    WordPress skill and Trail Depot project added

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -15,7 +15,7 @@ export const ProjectCard = ({project: {title, imageSrc, description, skills, dem
             </ul>
             <div className={styles.links}>
                 <a href={demo} className={styles.link} target="_blank">Demo</a>
-                <a href={source} className={styles.link} target="_blank">Source</a>
+                {source && <a href={source} className={styles.link} target="_blank">Source</a>}
             </div>
         </div>
     )
